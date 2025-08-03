c9e6de66bfa41239ffc7aa7f56e5c3acc569ded4
Antoine Paulet <antoinepaulet@orange.fr>
2025-08-03 18:21:24 +0200

    Reduce exported Mollweide outline thickness

--- script.js
+++ script.js
@@ -408,7 +408,7 @@ function createMollweideBorder(R = 100, thickness = 1, segments = 1024) {
   mesh.userData = {
     baseWidth: thickness,
     points: pts,
-    exportLineWidthFactor: 5,
+    exportLineWidthFactor: 3,
     baseRadius: R,
     segments,
     isMollweideBorder: true
