20f119bad00ca42cd48e519d78004e027ade9cbd
Antoine Paulet <antoinepaulet@orange.fr>
2025-08-03 19:57:35 +0700

    Increase exported border thickness

--- script.js
+++ script.js
@@ -398,7 +398,7 @@ function createMollweideBorder(R = 100, thickness = 1, segments = 1024) {
   });
   const line = new THREE.LineLoop(geometry, material);
   line.renderOrder = 1001;
-  line.userData = { baseLineWidth: thickness, exportLineWidthFactor: 3 };
+  line.userData = { baseLineWidth: thickness, exportLineWidthFactor: 6 };
   return line;
 }
 
